796ca24264eb78d8c921ab8a1e9d92d78413da38
Ignacio Solano <ignacioss_02@hotmail.com>
2014-12-15 19:07:26 -0600

    Al archivo se le modificaron las direcciones para cuando se mueva el Joystick en forma diagonal no leael valor de los voltajes de los dos ejes al mismo tiempo, si no que tenga una prioridad antes. Tambien se le quita el delay y los Serial.println para hacer mas eficiente el programa.

--- Arduino/Controlador_de_Joystick/Controlador_de_Joystick.ino
+++ Arduino/Controlador_de_Joystick/Controlador_de_Joystick.ino
@@ -7,67 +7,99 @@ int y;
 int x_cero;
 int y_cero;
 
-int Sensibilidad=100;
+int Sensibilidadx=400;
+int Sensibilidady=200;
 
 void evaluarjoystick(){
 
-  Serial.println();
+  //Serial.println();
   
   y= analogRead(A0);
-  Serial.println(y);
+  //Serial.println(y);
   
   x= analogRead(A1);
-  Serial.println(x);  
+  //Serial.println(x);  
   
-  if (x>x_cero+Sensibilidad){
+  if (x>x_cero+Sensibilidadx){
 
     Serial.println('d');
     
-  }
-
-  if (x<x_cero-Sensibilidad){
+  } else if (x<x_cero-Sensibilidadx){
 
     Serial.println('a');
 
-  }
+  } else
 
-  if (y>y_cero+Sensibilidad){
+  if (y>y_cero+Sensibilidady){
 
     Serial.println('w');
 
-  }
-
-  if (y<y_cero-Sensibilidad){
+  } else if (y<y_cero-Sensibilidady){
 
     Serial.println('s');
 
   }
-
-
-
-
 }
+
+void evaluarBotones(){
+
+  //Serial.println(); 
+
+  if (digitalRead(3)==LOW){
+    Serial.println("Start");
+  }
+  if (digitalRead(4)==LOW){
+    Serial.println("Pausa");
+  }
+if (digitalRead(5)==LOW){
+    Serial.println('L');
+  }
+if (digitalRead(6)==LOW){
+    Serial.println('K');
+  }
+if (digitalRead(7)==LOW){
+    Serial.println("J");
+  }  
+  if (digitalRead(8)==LOW){
+    Serial.println('I');
+  }
+  if (digitalRead(9)==LOW){
+    Serial.println('O');
+  }
+}
+
+
+
+
 
 void setup (){
 
-  Serial.println();
+  //Serial.println();
   
   y_cero= analogRead(A0);
-  Serial.println(y_cero);
+  //Serial.println(y_cero);
   
   x_cero= analogRead(A1);
-  Serial.println(x_cero);
+  //Serial.println(x_cero);
   
-  Serial.begin(9200);
-
-
+  Serial.begin(9600);
+  
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
+  
 }
 
 void loop (){
   
   evaluarjoystick();
   
-  delay (1000);
-
+  //delay (1000);
+  
+  evaluarBotones();
 }
 
